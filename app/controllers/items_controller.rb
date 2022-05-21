5df556fee64ba01ce85bdb4368e78eea4398a4e9
Kim G <xiaole.guo@yahoo.com>
2022-05-21 11:06:50 -0600

    complete item edit feature and add bootstrap styling

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-
+  before_action :set_item, only: [:edit, :update]
   def index
     @items = Item.all 
   end
@@ -10,16 +10,33 @@ class ItemsController < ApplicationController
   def create 
     new_item = Item.create(item_params)
     if new_item.save
-      redirect_to '/items'
+      redirect_to items_path
     else
-      redirect_to '/items/new'
+      redirect_to new_item_path
       flash[:alert] = "#{new_item.errors.full_messages.to_sentence}"
     end
   end 
 
+  def edit
+  end
+
+  def update
+    item_update = @item.update(item_params)
+    if item_update
+      redirect_to items_path
+    else
+      redirect_to edit_item_path(@item)
+      flash[:alert] = "#{@item.errors.full_messages.to_sentence}"
+    end
+
+  end
 
 private 
   def item_params
     params.permit(:name, :description, :quantity, :status)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
---
+++ spec/features/items/edit_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :feature do
+  let!(:item) {create(:item)}
+  it 'updates a new inventory item' do 
+
+    visit edit_item_path(item)
+
+
+
+    # within ".item-#{item.id}" do
+    #   click_on edit
+    # end
+    
+      fill_in :name, with: 'New Item'
+      fill_in :description, with: 'A description'
+      fill_in :quantity, with: 20
+      click_on 'Update Item'
+
+    expect(current_path).to eq(items_path)
+    expect(page).to have_content('New Item')
+    expect(page).to have_content('A description')
+    expect(page).to have_content(20)
+    expect(page).to have_content('In stock')
+    expect(page).to_not have_content(item.name)
+    expect(page).to_not have_content(item.description)
+    expect(page).to_not have_content(item.quantity)
+  end
+
+  it 'cannot update a new inventory item with missing input' do 
+    visit edit_item_path(item)
+
+    fill_in :name, with: ''
+    fill_in :description, with: ''
+    fill_in :quantity, with: ''
+    
+    click_on 'Update Item'
+
+    expect(current_path).to eq(edit_item_path(item))
+    expect(page).to have_content("Name can't be blank, Description can't be blank, Quantity can't be blank, Quantity is not a number, and Quantity is not a number")
+  end
+end
